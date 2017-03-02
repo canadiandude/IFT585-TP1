f81784a8ba7d83a95eca5879a303f73b3dff5586
Frank Rioux <francoisrioux67@hotmail.com>
2017-03-02 12:33:51 -0500

    Code Hamming part 2

--- TP1/TP1/Bits.cs
+++ TP1/TP1/Bits.cs
@@ -21,6 +21,20 @@ namespace TP1
             Array.Reverse(bits);
         }
 
+        public Bits() { }
+
+        public override string ToString()
+        {
+            String str = "";
+            foreach (bool b in bits) str += b ? "1" : "0";
+            return str;
+        }
+
+        public void Flip(int pos)
+        {
+            bits[pos] = !bits[pos];
+        }
+
         public static int BitLength(int num)
         {
             if (num == 0) return 1;
@@ -33,20 +47,27 @@ namespace TP1
             return count;
         }
 
-        public override string ToString()
+        public static bool Puissance2(int x)
         {
-            String str = "";
-            foreach (bool b in bits) str += b ? "1" : "0";
-            return str;
+            return (x & (x - 1)) == 0;
+        }
+
+        public static bool EstPair(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        public static Bits Codifier(int num)
+        {
+            return Codifier(new Bits(num));
         }
 
         public static Bits Codifier(Bits data)
         {
-            Bits code = new Bits(0);
+            Bits code = new Bits();
             code.bits = new bool[data.bits.Length + BitLength(data.bits.Length)];
 
-            int bitIndex = 0;
-            for (int i = 0; i < code.bits.Length; ++i)
+            for (int i = 0, bitIndex = 0; i < code.bits.Length; ++i)
             {
                 if (!Puissance2(i + 1))
                 {
@@ -60,20 +81,66 @@ namespace TP1
                 int sum = 0;
                 for (int i = 1; i <= code.bits.Length; ++i)
                 {
+                    // Pour Pos 1 : 1,3,5,7... et pour pos 2:s 2,3,6,7,10...
+                    // Tout les nombres qui ont ce bit allume
                     if ((i & parity) != 0 && code.bits[i - 1])
                     {
                         ++sum;
                     }
                 }
-                code.bits[parity - 1] = sum % 2 != 0;
+                code.bits[parity - 1] = !EstPair(sum);
             }
 
             return code;
         }
 
-        public static bool Puissance2(int x)
+        public static Bits Extraire(Bits code)
         {
-            return (x & (x - 1)) == 0;
+            Bits data = new Bits();
+            data.bits = new bool[code.bits.Length - BitLength(code.bits.Length)];
+
+            for (int i = 0, bitIndex = 0; i < code.bits.Length; ++i)
+            {
+                if (!Puissance2(i + 1))
+                {
+                    data.bits[bitIndex++] = code.bits[i];
+                }
+            }
+
+            return data;
+        }
+
+        public static int Verifier(Bits code)
+        {
+            int badBit = 0;
+
+            for (int p = 0; p < BitLength(code.bits.Length); ++p)
+            {
+                int parity = (int)Math.Pow(2, p);
+                int sum = 0;
+                for (int i = 1; i <= code.bits.Length; ++i)
+                {
+                    if (i != parity && (i & parity) != 0 && code.bits[i - 1])
+                    {
+                        ++sum;
+                    }
+                }
+
+                if (code.bits[parity - 1] == EstPair(sum))
+                {
+                    badBit += parity;
+                }
+            }
+
+            return badBit;
+        }
+
+        public static Bits Decoder(Bits code)
+        {
+            int badBit = Verifier(code);
+            if (badBit != 0)
+                code.Flip(badBit - 1);
+            return Extraire(code);
         }
     }
 }
